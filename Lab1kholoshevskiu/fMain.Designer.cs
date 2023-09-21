9b445a826c8146bdf704671e83ee001bcdafc7b9
Артем Холошевський <105263276+Arzenh@users.noreply.github.com>
2023-09-21 14:24:58 +0300

    Add project files.

---
+++ Lab1kholoshevskiu/fMain.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Lab1kholoshevskiu
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnMain = new Panel();
+            pnTools = new Panel();
+            btnRightFar = new Button();
+            btnRight = new Button();
+            btnLeft = new Button();
+            btnLeftFar = new Button();
+            btnDownFar = new Button();
+            btnDown = new Button();
+            btnCollapse = new Button();
+            btnExpand = new Button();
+            btnUp = new Button();
+            btnUpFar = new Button();
+            btnShow = new Button();
+            btnHide = new Button();
+            btnCreateNew = new Button();
+            cbCircles = new ComboBox();
+            label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            pnTools.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnMain
+            // 
+            pnMain.BackColor = Color.White;
+            pnMain.BorderStyle = BorderStyle.FixedSingle;
+            pnMain.Location = new Point(0, -1);
+            pnMain.Name = "pnMain";
+            pnMain.Size = new Size(889, 759);
+            pnMain.TabIndex = 0;
+            pnMain.Paint += pnMain_Paint;
+            // 
+            // pnTools
+            // 
+            pnTools.BackColor = Color.Gray;
+            pnTools.Controls.Add(btnRightFar);
+            pnTools.Controls.Add(btnRight);
+            pnTools.Controls.Add(btnLeft);
+            pnTools.Controls.Add(btnLeftFar);
+            pnTools.Controls.Add(btnDownFar);
+            pnTools.Controls.Add(btnDown);
+            pnTools.Controls.Add(btnCollapse);
+            pnTools.Controls.Add(btnExpand);
+            pnTools.Controls.Add(btnUp);
+            pnTools.Controls.Add(btnUpFar);
+            pnTools.Controls.Add(btnShow);
+            pnTools.Controls.Add(btnHide);
+            pnTools.Controls.Add(btnCreateNew);
+            pnTools.Controls.Add(cbCircles);
+            pnTools.Controls.Add(label1);
+            pnTools.Location = new Point(895, -1);
+            pnTools.Name = "pnTools";
+            pnTools.Size = new Size(269, 759);
+            pnTools.TabIndex = 1;
+            pnTools.Paint += pnTools_Paint;
+            // 
+            // btnRightFar
+            // 
+            btnRightFar.Location = new Point(214, 208);
+            btnRightFar.Name = "btnRightFar";
+            btnRightFar.Size = new Size(41, 61);
+            btnRightFar.TabIndex = 14;
+            btnRightFar.Text = "Right Far";
+            btnRightFar.UseVisualStyleBackColor = true;
+            btnRightFar.Click += btnRightFar_Click;
+            // 
+            // btnRight
+            // 
+            btnRight.Location = new Point(168, 208);
+            btnRight.Name = "btnRight";
+            btnRight.Size = new Size(40, 61);
+            btnRight.TabIndex = 13;
+            btnRight.Text = "Right";
+            btnRight.UseVisualStyleBackColor = true;
+            btnRight.Click += btnRight_Click;
+            // 
+            // btnLeft
+            // 
+            btnLeft.Location = new Point(47, 208);
+            btnLeft.Name = "btnLeft";
+            btnLeft.Size = new Size(35, 61);
+            btnLeft.TabIndex = 12;
+            btnLeft.Text = "Left";
+            btnLeft.UseVisualStyleBackColor = true;
+            btnLeft.Click += btnLeft_Click;
+            // 
+            // btnLeftFar
+            // 
+            btnLeftFar.Location = new Point(3, 210);
+            btnLeftFar.Name = "btnLeftFar";
+            btnLeftFar.Size = new Size(38, 61);
+            btnLeftFar.TabIndex = 11;
+            btnLeftFar.Text = "Left Far";
+            btnLeftFar.UseVisualStyleBackColor = true;
+            btnLeftFar.Click += btnLeftFar_Click;
+            // 
+            // btnDownFar
+            // 
+            btnDownFar.Location = new Point(90, 298);
+            btnDownFar.Name = "btnDownFar";
+            btnDownFar.Size = new Size(75, 23);
+            btnDownFar.TabIndex = 10;
+            btnDownFar.Text = "DownFar";
+            btnDownFar.UseVisualStyleBackColor = true;
+            btnDownFar.Click += btnDownFar_Click;
+            // 
+            // btnDown
+            // 
+            btnDown.Location = new Point(90, 269);
+            btnDown.Name = "btnDown";
+            btnDown.Size = new Size(75, 23);
+            btnDown.TabIndex = 9;
+            btnDown.Text = "Down";
+            btnDown.UseVisualStyleBackColor = true;
+            btnDown.Click += btnDown_Click;
+            // 
+            // btnCollapse
+            // 
+            btnCollapse.Location = new Point(90, 240);
+            btnCollapse.Name = "btnCollapse";
+            btnCollapse.Size = new Size(75, 23);
+            btnCollapse.TabIndex = 8;
+            btnCollapse.Text = "-";
+            btnCollapse.UseVisualStyleBackColor = true;
+            btnCollapse.Click += btnCollapse_Click;
+            // 
+            // btnExpand
+            // 
+            btnExpand.Location = new Point(90, 210);
+            btnExpand.Name = "btnExpand";
+            btnExpand.Size = new Size(75, 24);
+            btnExpand.TabIndex = 7;
+            btnExpand.Text = "+";
+            btnExpand.UseVisualStyleBackColor = true;
+            btnExpand.Click += btnExpand_Click;
+            // 
+            // btnUp
+            // 
+            btnUp.Location = new Point(90, 181);
+            btnUp.Name = "btnUp";
+            btnUp.Size = new Size(75, 23);
+            btnUp.TabIndex = 6;
+            btnUp.Text = "Up";
+            btnUp.UseVisualStyleBackColor = true;
+            btnUp.Click += btnUp_Click;
+            // 
+            // btnUpFar
+            // 
+            btnUpFar.Location = new Point(90, 152);
+            btnUpFar.Name = "btnUpFar";
+            btnUpFar.Size = new Size(75, 23);
+            btnUpFar.TabIndex = 5;
+            btnUpFar.Text = "UpFar";
+            btnUpFar.UseVisualStyleBackColor = true;
+            btnUpFar.Click += btnUpFar_Click;
+            // 
+            // btnShow
+            // 
+            btnShow.Location = new Point(36, 111);
+            btnShow.Name = "btnShow";
+            btnShow.Size = new Size(170, 23);
+            btnShow.TabIndex = 4;
+            btnShow.Text = "Показати об'єкт";
+            btnShow.UseVisualStyleBackColor = true;
+            btnShow.Click += btnShow_Click;
+            // 
+            // btnHide
+            // 
+            btnHide.Location = new Point(36, 82);
+            btnHide.Name = "btnHide";
+            btnHide.Size = new Size(170, 23);
+            btnHide.TabIndex = 3;
+            btnHide.Text = "Приховати об'єкт";
+            btnHide.UseVisualStyleBackColor = true;
+            btnHide.Click += btnHide_Click;
+            // 
+            // btnCreateNew
+            // 
+            btnCreateNew.Location = new Point(36, 53);
+            btnCreateNew.Name = "btnCreateNew";
+            btnCreateNew.Size = new Size(170, 23);
+            btnCreateNew.TabIndex = 2;
+            btnCreateNew.Text = "Створити новий об'єкт";
+            btnCreateNew.UseVisualStyleBackColor = true;
+            btnCreateNew.Click += btnCreateNew_Click;
+            // 
+            // cbCircles
+            // 
+            cbCircles.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbCircles.FormattingEnabled = true;
+            cbCircles.Location = new Point(17, 24);
+            cbCircles.Name = "cbCircles";
+            cbCircles.Size = new Size(205, 23);
+            cbCircles.TabIndex = 1;
+            cbCircles.SelectedIndexChanged += cbCircles_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Перелік об'єктів";
+            label1.Click += label1_Click;
+            // 
+            // backgroundWorker1
+            // 
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1162, 756);
+            Controls.Add(pnTools);
+            Controls.Add(pnMain);
+            Name = "fMain";
+            Text = "Лабараторна робота №1 Холошевський Артем";
+            Load += fMain_Load;
+            pnTools.ResumeLayout(false);
+            pnTools.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnMain;
+        private Panel pnTools;
+        private Button btnShow;
+        private Button btnHide;
+        private Button btnCreateNew;
+        private ComboBox cbCircles;
+        private Label label1;
+        private Button btnLeft;
+        private Button btnLeftFar;
+        private Button btnDownFar;
+        private Button btnDown;
+        private Button btnCollapse;
+        private Button btnExpand;
+        private Button btnUp;
+        private Button btnUpFar;
+        private Button btnRightFar;
+        private Button btnRight;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ Lab1kholoshevskiu/fMain.cs
@@ -0,0 +1,212 @@
+using System.DirectoryServices.ActiveDirectory;
+
+namespace Lab1kholoshevskiu
+{
+    public partial class fMain : Form
+    {
+        CEmblem[] circles;
+        CEmblem[] squares;
+        CEmblem[] triagles;
+        int CircleCount = 0;
+        int SquareCount = 0;
+        int TriagleCount = 0;
+        int CurrentCircleIndex;
+        int CurrentSquareIndex;
+        int CurrentTriagleIndex;
+
+
+        public fMain()
+        {
+
+
+            {
+                InitializeComponent();
+                // Створення масиву для об'єктів - кіл
+                circles = new CEmblem[100];
+                squares = new CEmblem[100];
+                triagles = new CEmblem[100];
+            }
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btnCreateNew_Click(object sender, EventArgs e)
+        {
+            if (CircleCount >= 99)
+            {
+                MessageBox.Show("Досягнуто межі кількості об'єктів!");
+                return;
+            }
+
+
+
+
+
+            Graphics graphics = pnMain.CreateGraphics();
+            CurrentCircleIndex = CircleCount;
+
+
+            // Створення нового об'єкта - екземпляра класу CCircle
+            circles[CurrentCircleIndex] =
+             new CEmblem(graphics, pnMain.Width / 2,
+             pnMain.Height / 2, 50);
+            circles[CurrentCircleIndex].Show();
+            CircleCount++;
+            cbCircles.Items.Add("об'єкт No" + (CircleCount - 1).ToString());
+            cbCircles.SelectedIndex = CircleCount - 1;
+
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Hide();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Відображення поточного об'єкта
+            circles[CurrentCircleIndex].Show();
+        }
+
+        private void btnExpand_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Розширення поточного об'єкта - екземпляра класу CCircle
+            circles[CurrentCircleIndex].Expand(5);
+        }
+
+        private void btnCollapse_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Стискання поточного об'єкта - екземпляра класу CCircle
+            circles[CurrentCircleIndex].Collapse(5);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення вверх поточного об'єкта
+            circles[CurrentCircleIndex].Move(0, -10);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення вниз поточного об'єкта
+            circles[CurrentCircleIndex].Move(0, 10);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення вправо поточного об'єкта
+            circles[CurrentCircleIndex].Move(10, 0);
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення вліво поточного об'єкта
+            circles[CurrentCircleIndex].Move(-10, 0);
+        }
+
+        private void btnRightFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення поточного об'єкта вправо на більшу відстань
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void btnLeftFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення поточного об'єкта вліво на більшу відстань
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(-1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void btnUpFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення поточного об'єкта вверх на більшу відстань
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(0, -1);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void btnDownFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex > CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            // Переміщення поточного об'єкта вниз на більшу відстань
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(0, 1);
+                System.Threading.Thread.Sleep(5);
+
+            }
+        }
+
+       
+
+        private void pnMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnTools_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void cbCircles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+        }
+    }
+}
